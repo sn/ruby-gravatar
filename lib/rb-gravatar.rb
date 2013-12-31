e382c679407049c2a3bac343afbc25b8124a9fc3
Sean Nieuwoudt <sean@isean.co.za>
2013-12-31 15:31:18 +0200

    Handling nil email addresses

--- lib/rb-gravatar.rb
+++ lib/rb-gravatar.rb
@@ -2,10 +2,12 @@ require 'digest/md5'
 require 'cgi'
 
 class Gravatar
+  # Construct the Gravatar URI generically
   def self.construct_resource(email_address, size, default)
     if size < 1 || size > 2048
       size = 64
     end
+    
     parts = []
     parts << "http://www.gravatar.com/avatar/"
     parts << "#{Digest::MD5.hexdigest(email_address.downcase.strip)}"
@@ -18,12 +20,22 @@ class Gravatar
     parts.join        
   end
   
+  # Generate and return only the Gravatar URI
   def self.src(email_address, size = 64, default = nil)
-    Gravatar.construct_resource(email_address, size, default)
+    begin
+      Gravatar.construct_resource(email_address, size, default)
+    rescue Exception => e
+      nil
+    end
   end
 
+  # Generate and return the full img tag for the Gravatar URI
   def self.tag(email_address, size = 64, default = nil, alt_text = nil)    
-    return "<img src='#{Gravatar.construct_resource(email_address, size, default)}' />" if alt_text.nil?
-    return "<img src='#{Gravatar.construct_resource(email_address, size, default)}' alt='#{alt_text}' />" if !alt_text.nil?
+    begin
+      return "<img src='#{Gravatar.construct_resource(email_address, size, default)}' class='gravatar' />" if alt_text.nil?
+      return "<img src='#{Gravatar.construct_resource(email_address, size, default)}' class='gravatar' alt='#{alt_text}' />" if !alt_text.nil?      
+    rescue Exception => e
+      nil
+    end
   end  
 end
